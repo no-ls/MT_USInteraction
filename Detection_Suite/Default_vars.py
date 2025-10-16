66662c281eeab224b5fa5cea5910c7db7c527bb3
Leonie <leonie.schrod@t-online.de>
2025-10-16 11:55:08 +0200

    create minimal working prototype + 2 algorithms

---
+++ Detection_Suite/Default_vars.py
@@ -0,0 +1,27 @@
+class KEYS:
+    ESC         = 27
+    SPACE       = 32
+    ZERO        = 48
+    ONE         = 49
+    TWO         = 50
+    THREE       = 51
+    FOUR        = 52
+    FIVE        = 53
+    SIX         = 54
+    SEVEN       = 55
+    EIGHT       = 56
+    NINE        = 57
+    LEFT_ARROW  = 2424832
+    UP_ARROW    = 2490368
+    RIGHT_ARROW = 2555904
+    DOWN_ARROW  = 2621440
+
+class COLORS:
+    WHITE   = (255, 255, 255)
+    BLACK   = (0, 0, 0)
+    BLUE    = (255, 0, 0)
+    GREEN   = (0, 255, 0)
+    RED     = (0, 0, 255)
+
+class DETECTION_DEFAULTS:
+    THRESHOLD = 30
---
+++ Detection_Suite/main.py
@@ -0,0 +1,186 @@
+import cv2
+from cv2.typing import MatLike
+import argparse
+
+from Default_vars import COLORS, KEYS
+from Detectors import Algorithm, ALGORITHMS
+# from detectors import Hallo
+# Main Module for complete Detection Suit
+# implement algorithms in different modules
+# evtl. implement quality metrics for comparing
+
+WINDOW = "Detection Suit"
+DEFAULT_IMG = "../Data/Agar1.png"
+DEFAULT_VID = "../Data/Lego.mp4"
+
+COLOR_MAX = 255
+US_AREA_THRESHOLD = 30
+
+class CL_Parser():
+    """
+    Read and parse the command line parameters.
+    See: https://docs.python.org/3/library/argparse.html
+    """
+
+    def __init__(self) -> None:
+        self.path = DEFAULT_IMG
+        self.parser = argparse.ArgumentParser(
+            prog="US_Detection_Suite",
+            formatter_class=argparse.RawDescriptionHelpFormatter,
+            description="Suite of different CV detection algorithms for Ultrasound Images",
+            # epilog="""TODO"""
+        )
+
+        self.parser.add_argument('-src', '--source', type=str,
+            default=DEFAULT_IMG, required=False, action="store",
+            help="""The path to the image/video-file you want to use"""
+        )
+
+        # TODO pass debug as a cli arg
+
+        self.parse_cmd_input()
+
+    def parse_cmd_input(self):
+        args = self.parser.parse_args()
+        if args.source:
+            self.path = args.source  
+        print(f"[INFO] --> source: {self.path}")
+
+class US_Area():
+    """The area of the image/video that contains only the ultrasound scan"""
+    def __init__(self, x, y, w, h) -> None:
+        self.x = x # top right
+        self.y = y # top right
+        self.w = w
+        self.h = h 
+
+class Coordinator():
+    """Coordinate between the different algorithms and settings"""
+
+    def __init__(self, debug=False) -> None:
+        self.debug = debug
+        self.crop_min = 30
+        self.algorithm = ALGORITHMS[KEYS.ONE] # default init
+        self.value = 50 # TODO: init value depending on detection algorithm
+
+    def manage(self, src:MatLike) -> MatLike:
+        """Manage all processing that is happening to an image and return it"""
+        img, gray = self.preprocess_image(src)
+
+        result = self.apply_algorithm(self.algorithm, self.value, img, gray)
+
+        out = self.split_screen(self.debug, img, result)
+        return out
+
+    def preprocess_image(self, src:MatLike) -> tuple[MatLike]:
+        """returns the preprocessed image as both BGR and Grayscale"""
+        src = cv2.rotate(src, cv2.ROTATE_90_CLOCKWISE)
+        gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
+        img = self.find_US_area(src, gray)
+        return (img, gray)
+    
+    def split_screen(self, show:bool, src:MatLike, out:MatLike) -> MatLike:
+        """Create a split screen from two frames to show them side by side"""
+        if show:
+            if(len(out.shape) < 3):
+                out = cv2.cvtColor(out, cv2.COLOR_GRAY2BGR)
+            return cv2.hconcat([src, out])
+        return src
+
+    def apply_algorithm(self, algorithm: Algorithm, value, img, gray):
+        return algorithm.apply(value, img, gray)
+
+    def compare_algorithms(self):
+        # get the 2 chosen algorithms and compare their output (Split Screen)
+        pass
+
+    def change_value(self, key):
+        # TODO: make sure it can't overflow
+        if key == KEYS.UP_ARROW:
+            self.value += 1
+        else:
+            self.value -= 1
+
+    def change_algorithm(self, key):
+        """Switch between the implemented algorithms using the number keys 0-9 (not numpad)"""
+        try:
+            self.algorithm = ALGORITHMS[key]
+        except:
+            print("[ERR] Nothing algorithm here yet")
+    
+    # ----- HELPERS -----
+
+    def find_US_area(self, src:MatLike, gray:MatLike) -> MatLike:
+        """Find the original image so only the Ultrasound scan area is left"""
+        
+        _, thresh = cv2.threshold(gray, self.crop_min, 255, cv2.THRESH_BINARY)
+        eroded = cv2.erode(thresh, None, iterations=2) # get rid of text/lines
+        contours, _ = cv2.findContours(eroded, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+        x, y, w, h = cv2.boundingRect(max(contours, key=cv2.contourArea))
+
+        if self.debug:
+            cv2.rectangle(src, (x, y), (x + w, y + h), COLORS.GREEN, 1)            
+
+        self.area = US_Area(x, y, w, h)
+        return src
+
+
+class Viewer():
+    """Handle all CV2 functions, that affect viewing the data"""
+
+    def __init__(self, debug:bool=False) -> None:
+        self.debug = debug
+        self.parser = CL_Parser()
+        self.coordinator = Coordinator(self.debug)
+        self.load_data()
+
+    def load_data(self):
+        if ".mp4" in self.parser.path:
+            cap = cv2.VideoCapture(self.parser.path)
+            self.show_video(cap)
+        else:
+            frame= cv2.imread(self.parser.path)
+            self.show_img(frame)
+        # evtl. open with mouse after running program (-> from tkinter import filedialog)
+
+    def show_img(self, frame:MatLike) -> None:
+        out = self.coordinator.manage(frame)
+        cv2.imshow(WINDOW, out)
+        cv2.waitKey(0)
+
+    def show_video(self, cap:cv2.VideoCapture) -> None:
+        while cap.isOpened():
+            ret, frame = cap.read()
+            if not ret:
+                break
+
+            key = cv2.waitKeyEx(25)
+            # if key != -1: print(key)
+            if key == ord("q") or key == KEYS.ESC:
+                self.stop(cap)
+            elif key == KEYS.SPACE:
+                self.pause()
+            elif key == KEYS.UP_ARROW or key == KEYS.DOWN_ARROW:
+                self.coordinator.change_value(key)
+            elif key >= KEYS.ZERO and key <= KEYS.NINE:
+                self.coordinator.change_algorithm(key)
+            
+            out = self.coordinator.manage(frame)
+            cv2.imshow('Video Playback', out)
+
+    def stop(self, cap:cv2.VideoCapture):
+        cv2.destroyAllWindows()
+        cap.release()
+
+    def pause(self):
+        cv2.waitKey()
+        
+
+# ----- MAIN ----- #
+def main():
+    # TODO: put detection algorithms on number keys for quick change between
+    viewer = Viewer(True)
+    # coordinator = Coordinator(True)
+    # coordinator.preprocess_image(viewer.frame)
+    
+main()
