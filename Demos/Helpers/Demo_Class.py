2caece75cd2f00e4cd5ddbbcc70cc53df3f45c27
Leonie <leonie.schrod@t-online.de>
2025-10-21 14:58:22 +0200

    add fps display and  mask US ROI (and probe artifact area (top "row"))

--- Demos/Helpers/Demo_Class.py
+++ Demos/Helpers/Demo_Class.py
@@ -1,5 +1,10 @@
+import cv2
+import time
+from .Parameters import COLORS
 from abc import abstractmethod
 from cv2.typing import MatLike
+
+FPS_POS = (10, 20)
 
 class Demo():
     def __init__(self) -> None:
@@ -7,6 +12,18 @@ class Demo():
 
     def get_name(self):
         return self.__class__.__name__
+    
+    def show_fps(self, frame:MatLike)->MatLike:
+        self.new_frame_time = time.time()
+        try:
+            fps = 1 / (self.new_frame_time - self.prev_frame_time)
+            fps = int(fps)
+        except:
+            fps = "x"
+        self.prev_frame_time = self.new_frame_time
+        text = f"fps: {str(fps)}"
+        cv2.putText(frame, text, FPS_POS, cv2.FONT_HERSHEY_PLAIN, 1, COLORS.WHITE, 1, cv2.LINE_AA)    
+
     
     @abstractmethod
     def do(self, frame:MatLike, gray:MatLike)-> MatLike:
--- Demos/Helpers/Player.py
+++ Demos/Helpers/Player.py
@@ -1,7 +1,8 @@
-from abc import abstractmethod
 import cv2
+import numpy as np
+from abc import abstractmethod
 from cv2.typing import MatLike
-from .Parameters import KEYS
+from .Parameters import KEYS, COLORS
 from .Demo_Class import Demo
 
 VIDEO_ID = 2 # video id for the virtual camera
@@ -9,34 +10,52 @@ DEFAULT_VID = "../Data/Finger1.mp4"
 
 WINDOW = "Painter"
 US_AREA_THRESHOLD = 30
+PROBE_ARTIFACT = 10
 
 # ----- HELPERS ----- #
 
 class US_Area():
     """The area of the image/video that contains only the ultrasound scan"""
-    def __init__(self, x, y, w, h) -> None:
-        self.x = x # top right
-        self.y = y # top right
-        self.w = w
-        self.h = h 
+    def __init__(self) -> None:
+        # self.x = x # top right
+        # self.y = y # top right
+        # self.w = w
+        # self.h = h
+        pass
 
-def find_US_area(gray:MatLike) -> MatLike:
-    """Find the original image so only the Ultrasound scan area is left"""
+    def find_US_area(self, gray:MatLike):
+        """Find the original image so only the Ultrasound scan area is left""" 
+        _, thresh = cv2.threshold(gray, US_AREA_THRESHOLD, 255, cv2.THRESH_BINARY)
+        eroded = cv2.erode(thresh, None, iterations=2) # get rid of text/lines
+        contours, _ = cv2.findContours(eroded, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+        self.x, self.y, self.w, self.h = cv2.boundingRect(max(contours, key=cv2.contourArea))
     
-    _, thresh = cv2.threshold(gray, US_AREA_THRESHOLD, 255, cv2.THRESH_BINARY)
-    eroded = cv2.erode(thresh, None, iterations=2) # get rid of text/lines
-    contours, _ = cv2.findContours(eroded, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-    x, y, w, h = cv2.boundingRect(max(contours, key=cv2.contourArea))
+    def mask_US_area(self, img):
+        """Return a copy of the image that is masked to the US-Area (rest is black)"""
+        black = np.zeros((img.shape[0], img.shape[1], 3), np.uint8)
+        black = cv2.cvtColor(black, cv2.COLOR_BGR2GRAY)
+        cv2.rectangle(black, (self.x, self.y), (self.x+self.w, self.y+self.h), COLORS.WHITE, -1)
+        cv2.rectangle(black, (self.x, self.y), (self.x+self.w, self.y+PROBE_ARTIFACT), COLORS.BLACK, -1)
+        # -> remove the very top of the area, as there are a lot of probe artifacts and nothing else
+        
+        # HACK: remove bottom of area for testing
+        cv2.rectangle(black, (self.x, self.y+self.h-PROBE_ARTIFACT), (self.x+self.w, self.h+self.h), COLORS.BLACK, -1)
 
-    return US_Area(x, y, w, h)
+        _, mask = cv2.threshold(black, 127, 255, 0)
+        return cv2.bitwise_and(img, img, mask=mask)
+        # via: https://stackoverflow.com/a/42007566
+        # ROI = image[y:y+h, x:x+w] (https://stackoverflow.com/a/58177717) 
+
 
 
 # ----- PLAYER ----- # 
 
 class Player():
-    def __init__(self, demo:Demo) -> None:
+    def __init__(self, demo:Demo, video:str=DEFAULT_VID) -> None:
         self.demo = demo
         self.area = None
+        self.video = video
+        self.area = US_Area()
 
     def start_player(self):
         cap = self.load_input()
@@ -46,7 +65,7 @@ class Player():
         cap = cv2.VideoCapture(VIDEO_ID)
         if not cap.isOpened():
             print("[INFO] - could not find a stream, will use video")
-            cap = cv2.VideoCapture(DEFAULT_VID)
+            cap = cv2.VideoCapture(self.video)
         return cap
     
     def play_video(self, cap:cv2.VideoCapture):
@@ -59,8 +78,8 @@ class Player():
             if key == ord("q") or key == KEYS.ESC:
                 break
 
-            frame, gray = self.prepare_video(frame)
-            out = self.do_demo(frame, gray)
+            frame, prepped = self.prepare_video(frame)
+            out = self.do_demo(frame, prepped)
 
             cv2.imshow(WINDOW, out)
         
@@ -71,8 +90,9 @@ class Player():
         """Rotate the video and find the US area"""
         src = cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
         gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
-        self.area = find_US_area(gray)
-        return (src, gray)
+        self.area.find_US_area(gray)
+        roi = self.area.mask_US_area(gray)
+        return (src, roi)
     
     def do_demo(self, frame:MatLike, gray:MatLike):
         return self.demo.do(frame, gray)
