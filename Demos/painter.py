dd4cc7a30f731a27ad7254d4b2140660b8aa18d9
Leonie <leonie.schrod@t-online.de>
2025-12-02 14:25:28 +0100

    update: include line stops

--- Demos/painter.py
+++ Demos/painter.py
@@ -14,54 +14,78 @@ BBOX_MIN_H = 10
 DEFAULT_THRESH = 130
 THRESH_MAX = 255
 
+# NOTE: some videos work better with threshold, some with color quantization
+
 class Painter(Demo):
     def __init__(self) -> None:
         super().__init__()
-        self.lines = []
-        self.slider_val = DEFAULT_THRESH
+        self.slider_value = DEFAULT_THRESH
         self.slider_max = THRESH_MAX
         self.slider_name = "Threshold"
+        self.is_newline = True
+        self.lines: list[Line] = []
+        self.current_line: Line = None
 
-    def do(self, frame:MatLike, gray:MatLike)-> MatLike:
-        super().do(frame, gray) 
-        # self.show_fps(frame)
+    def do(self, frame:MatLike, masked:MatLike)-> MatLike:
+        super().do(frame, masked)
 
-        # blur image?
-        # gray = cv2.bilateralFilter(gray,9,75,75)
-        # gray = cv2.medianBlur(gray, 5)
-
-        # NOTE threshold might change for videos
-        _, thresh = cv2.threshold(gray, self.slider_val, THRESH_MAX, cv2.THRESH_BINARY)
+        # NOTE threshold might work better for some videos
+        gray = cv2.cvtColor(masked, cv2.COLOR_BGR2GRAY)
+        _, thresh = cv2.threshold(gray, self.slider_value, THRESH_MAX, cv2.THRESH_BINARY)
         contours, _ = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        x, y, w, h = cv2.boundingRect(max(contours, key=cv2.contourArea))
+        
+        # contours, frame = self.segment(masked)
 
-        # draw bounding box
-        if w > BBOX_MIN_W and h > BBOX_MIN_H:
-            cv2.rectangle(frame, (x, y), (x + w, y + h), COLORS.GREEN, 1)
+        if len(contours) == 0:
+            self.is_newline = True
+        else:
+            x, y, w, h = cv2.boundingRect(max(contours, key=cv2.contourArea))
+            cv2.drawContours(frame, contours, -1, COLORS.PURPLE, 1)
 
-            # draw center 
-            center = (round(x + w/2), round(y + h/2))
-            cv2.circle(frame, center, 5, COLORS.GREEN, -1)
-            self.lines.append(center)
+            if w > BBOX_MIN_W and h > BBOX_MIN_H:
+                # find the center
+                cv2.rectangle(frame, (x, y), (x + w, y + h), COLORS.GREEN, 1)
+                center = (round(x + w/2), round(y + h/2))
+                cv2.circle(frame, center, 5, COLORS.GREEN, -1)
 
+                if self.is_newline:
+                    self.current_line = Line()
+                    self.lines.append(self.current_line)
+                    self.is_newline = False
+
+                self.current_line.add_point(center)
+        
         self.draw_lines(frame)
         return frame
 
     def draw_lines(self, frame:MatLike):
-        if len(self.lines) == 0:
-            return
-        
-        # TODO: restart line, if finger removed
-        
-        for i in range (0, len(self.lines)-1):
-            start = self.lines[i]
-            end = self.lines[i+1]
-            cv2.line(frame, start, end, COLORS.RED, 3)
+        if len(self.lines) == 0: return
 
+        for line in self.lines:
+            line.draw(frame)
+
+class Line():
+    """Handles operations on all points of a single line"""
+
+    def __init__(self) -> None:
+        self.points = []
+
+    def add_point(self, point:tuple[int]):
+        self.points.append(point)
+
+    def draw(self, frame):
+        if len(self.points) == 0:
+            return
+
+        for i in range (0, len(self.points)-1):
+            start = self.points[i]
+            end = self.points[i+1]
+            cv2.line(frame, start, end, COLORS.RED, 3)
+ 
 
 # ----- MAIN ----- #
 video = "../Data/heart.mp4"
 # video = "../Data/zigzag.mp4"
-# video = "../Data/smiley.mp4"
+# video = "../Data/draw-triangle.mp4"
 player = Player(Painter(), video)
 player.start_player()
